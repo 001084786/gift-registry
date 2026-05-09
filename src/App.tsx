77ed0a5fb377f79a7f50befec150f0601842c15f
001084786 <nugget.micallef@gmail.com>
2026-05-10 02:53:58 +0930

    Setup routes to use repo name

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,11 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 
-           This defines the layout for the root route ("/").
-           Anything at "/" will be inside RootLayout, 
-           and HomePage component will render where <Outlet /> was.
-        */}
-        <Route element={<RootLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<HomePage />} />
-          
-          {/* You can have other routes that share the same layout */}
-          <Route path="/about" element={<AboutPage />} /> 
+        <Route path="/gift-registry" element={<RootLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
         </Route>
-
-        {/* 
-           Optional: Routes that don't need a header/footer (e.g., login screen)
-        */}
-        {/* <Route path="/login" element={<LoginPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
